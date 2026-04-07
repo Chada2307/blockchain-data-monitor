edc8e2e0f53fcff51624f24b4238db722cba114c
Adrian <adrian.bielenik@gmail.com>
2026-04-07 16:07:27 +0200

    initial commit

---
+++ src/main/java/ekipa/einsteina/monitor/MonitorBlockchainApplication.java
@@ -0,0 +1,13 @@
+package ekipa.einsteina.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorBlockchainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorBlockchainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ekipa/einsteina/monitor/MonitorBlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package ekipa.einsteina.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorBlockchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
