c22f286924fa9ecf560073b495bbddb64aad2c41
Adrian <adrian.bielenik@gmail.com>
2026-04-27 20:12:54 +0200

    B2.7 WSS feature added

--- src/main/java/ekipa/einsteina/monitor/access/AccessConfig.java
+++ src/main/java/ekipa/einsteina/monitor/access/AccessConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.websocket.WebSocketService;
+
+import java.io.IOException;
 
 @Configuration
 public class AccessConfig {
@@ -12,8 +15,17 @@ public class AccessConfig {
     @Value("${blockchain.node.url_https}")
     private String node_url_https;
 
+    @Value("${blockchain.node.url_wss}")
+    private String node_url_wss;
+
     @Bean
-    public Web3j web3j(){
-        return Web3j.build(new HttpService(node_url_https));
+    public Web3j web3j()throws Exception {
+        //return Web3j.build(new HttpService(node_url_https));
+        WebSocketService wss = new WebSocketService(node_url_wss, true);
+        Thread.sleep(3000);
+        wss.connect();
+
+
+        return Web3j.build(wss);
     }
 }
--- src/main/java/ekipa/einsteina/monitor/logic/MonitorService.java
+++ src/main/java/ekipa/einsteina/monitor/logic/MonitorService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.EthEstimateGas;
+import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
+import org.web3j.protocol.core.methods.response.EthTransaction;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -17,41 +20,95 @@ import java.util.stream.Stream;
 public class MonitorService {
     private final Web3j web3j;
     private final reportingService reporter;
-    private BigInteger lastBlockNumber = BigInteger.ZERO;
+    private int totalTrans = 0;
+    private int totalBlocks = 0;
 
     @Autowired
     public MonitorService(Web3j web3j, reportingService reporter){
         this.web3j = web3j;
         this.reporter = reporter;
+        initHistoricalBlocks();
+        startMonitorWSS();
     }
 
-    @Scheduled(fixedRate = 10000)
-    public void monitor(){
-        try{
-            BigInteger currentBlock = web3j.ethBlockNumber().send().getBlockNumber();
+    private void startMonitorWSS(){
+        web3j.blockFlowable(false).subscribe(ethBlock -> {
+            BigInteger number = ethBlock.getBlock().getNumber();
+            processSingleBlock(number, false);
+        }, error -> {
+            System.out.println("error: " + error.getMessage());
+        });
+    }
 
-            if(!currentBlock.equals(lastBlockNumber)){
-                processBlock(currentBlock);
-                lastBlockNumber = currentBlock;
+    public void processSingleBlock(BigInteger number, boolean isdetailed) throws IOException{
+        EthBlock block = web3j.ethGetBlockByNumber(DefaultBlockParameter.valueOf(number), true).send();
+
+        if(block.getBlock() != null){
+            totalBlocks++;
+            int txCount = block.getBlock().getTransactions().size();
+            totalTrans += txCount;
+            reporter.reportBlock(number, block.getBlock().getHash(), txCount);
+            if (isdetailed){
+                processTrans(block.getBlock().getTransactions());
+            }
+        }
+//
+//        int txCount = block.getBlock().getTransactions().size();
+//        System.out.println("-----------------------------");
+//        System.out.println("txCount: " + txCount);
+//        System.out.println("block: " + block.getBlock().getHash());
+//        System.out.println("-----------------------------");
+//        totalBlocks++;
+    }
+
+
+   public void initHistoricalBlocks(){
+        new Thread(() -> {
+            try{
+                BigInteger currentBlock = web3j.ethBlockNumber().send().getBlockNumber();
+
+                for (int i = 0; i < 100; i++){
+                    BigInteger blockNumber = currentBlock.subtract(BigInteger.valueOf(i));
+                    processSingleBlock(blockNumber, i < 10);
+                    Thread.sleep(300);
+                }
+
+            }catch(Exception e){
+                System.err.println("Monitor Error: " + e.getMessage());
+            }
+        }).start();
+    }
+
+    private void processTrans(List<EthBlock.TransactionResult> transactions){
+        transactions.forEach(txResult -> {
+            EthBlock.TransactionObject tx = (EthBlock.TransactionObject) txResult.get();
+
+
+            BigInteger actualGasUser = BigInteger.ZERO;
+            try{
+                var receipt = web3j.ethGetTransactionReceipt(tx.getHash()).send();
+                actualGasUser = receipt.getTransactionReceipt().map(r -> r.getGasUsed()).orElse(BigInteger.ZERO);
+            }catch (IOException e){
+                e.printStackTrace();
             }
 
-        }catch(IOException | InterruptedException e){
-            System.err.println("Monitor Error: " + e.getMessage());
-        }
+//            System.out.println("-----------------------------");
+//            System.out.println("Hash: " + tx.getHash());
+//            System.out.println("From: " + tx.getFrom());
+//            System.out.println("To: " + tx.getTo());
+//            System.out.println("Value: " + tx.getValue());
+//            System.out.println("Gas: " + actualGasUser);
+//            System.out.println("-----------------------------");
+
+            reporter.reportTrans(
+                    tx.getHash(),
+                    tx.getFrom(),
+                    tx.getTo(),
+                    tx.getValue(),
+                    actualGasUser
+            );
+        });
     }
 
-    private void processBlock(BigInteger currentBlock) throws InterruptedException, IOException{
-        List<BigInteger> blockNumbers = Stream.iterate(currentBlock, x -> x.subtract(BigInteger.ONE))
-                .limit(100)
-                .toList();
-
-        for(BigInteger blockNumber : blockNumbers){
-            EthBlock ethBlock = web3j.ethGetBlockByNumber(DefaultBlockParameter.valueOf(blockNumber), true).send();
-            System.out.println("Block Number: " + blockNumber);
-            System.out.println("Block: " + ethBlock.getBlock().getHash());
-            System.out.println("Trans: " + ethBlock.getBlock().getTransactions().size());
-            reporter.reportToFile(blockNumber, ethBlock.getBlock().getHash(), ethBlock.getBlock().getTransactions().size());
-        }
-    }
 
 }
