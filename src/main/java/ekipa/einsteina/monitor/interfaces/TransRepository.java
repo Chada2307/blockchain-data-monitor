180f96d1a91de369b5f6a7fb0931040c37f52d9d
Adrian <adrian.bielenik@gmail.com>
2026-05-07 18:45:21 +0200

    B3.1 simple api implementation

--- src/main/java/ekipa/einsteina/monitor/interfaces/TransRepository.java
+++ src/main/java/ekipa/einsteina/monitor/interfaces/TransRepository.java
@@ -2,6 +2,7 @@ package ekipa.einsteina.monitor.interfaces;
 
 import ekipa.einsteina.monitor.Models.TransEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface TransRepository extends JpaRepository<TransEntity, String> {
     List<TransEntity> findByFromAddress(String fromAddress);
     List<TransEntity> findByToAddress(String toAddress);
 
-
+    @Query("SELECT AVG(t.gasUsed) FROM TransEntity t")
+    Double findAverageGasUsed();
 }
